4b69022ea41c94f897249f69f92b064a3696adbc
maribelcuales <maribelcuales@gmail.com>
2020-08-22 18:30:49 +0800

    Added SQL command select to get endpoint.

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,6 +6,20 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  // get a list of posts from the database
+  // select * from posts;
+  db.select("*")
+    .from("posts")
+    .then(posts => {
+      res.status(200).json({ data: posts });
+    })
+    .catch(error => {
+    //save it to a log somewhere
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  });
+
+  // respond with the posts and 200
 
 });
 
