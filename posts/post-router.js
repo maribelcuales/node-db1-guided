3fdb9b6d7fb464da1cf343ef2610c02b13d45bd2
maribelcuales <maribelcuales@gmail.com>
2020-08-23 09:38:57 +0800

    Implement GET /api/posts/:id endpoint.

--- posts/post-router.js
+++ posts/post-router.js
@@ -14,17 +14,29 @@ router.get('/', (req, res) => {
       res.status(200).json({ data: posts });
     })
     .catch(error => {
-    //save it to a log somewhere
-    console.log(error);
-    res.status(500).json({ message: error.message });
-  });
-
-  // respond with the posts and 200
-
+      //save it to a log somewhere
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    });
 });
 
 router.get('/:id', (req, res) => {
-
+  // use the docs http://knexjs.org/#Builder-where to implement the GET /api/posts/:id endpoint 
+  db("posts")
+    .where({ id: req.params.id })
+    .first() // pick the first record from the array 
+    .then(post => {
+      if(post) {
+        res.status(200).json({ data: post });
+      } else {
+        res.status(404).json({ message: "No posts by that ID" });
+      }
+    })
+    .catch(error => {
+      //save it to a log somewhere
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    });
 });
 
 router.post('/', (req, res) => {
